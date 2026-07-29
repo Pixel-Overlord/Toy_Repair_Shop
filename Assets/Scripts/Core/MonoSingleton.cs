a91a8e393c6ec9d0c773e9b8363929abc44eadaa
Satish Jha <jha.satish0210@gmail.com>
2026-07-29 14:55:04 +0530

    Initial scripts added

---
+++ Assets/Scripts/Core/MonoSingleton.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace ToyRepairShop.Core
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+    {
+        public static T Instance { get; private set; }
+
+        protected virtual void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = (T)this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
